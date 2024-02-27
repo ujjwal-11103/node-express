4a940089689e4224c0b482391fd7708380a30f77
Ujjwal Upadhyay <ujjwalupadhyay2003@gmail.com>
2024-02-27 10:49:18 +0530

    error handling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const port = 3000;      // we will use this later
 
 //code for creating a server
 app.get('/', (req, res) => {
-    res.send('Hello, wosdfdsrld!');
+    res.send('Hello, World!');
     console.log(`Server listening at http://localhost:${port}`);
 });
 
@@ -18,9 +18,6 @@ app.get('/profile/:username', function (req, res) {
     res.send(`hello from ${req.params.username}`);
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
 
 
 app.set('view engine', 'ejs');
@@ -29,8 +26,25 @@ app.get("/index", function (req, res) {
     res.render("index", { name: "ujjwal" })
 })
 
+app.use(express.static("./public"));  //static file setup
+
+app.get("/error", function (req, res) {
+    throw Error("Something went wrong");
+})
+
+// error handling
+app.use(function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+})
 
 
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
 // app.listen(3000); // this is the same as the above line
 
 
